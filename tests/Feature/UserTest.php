15d8fcc012d975e8c1ca57b8f454ec45018a932b
andrea <m2000018327@webmail.uad.ac.id>
2023-09-11 12:39:01 +0700

    Changes to be committed: 	modified:   app/Http/Controllers/UserController.php 	new file:   app/Http/Requests/UserLoginRequest.php 	modified:   app/Http/Resources/UserResource.php 	modified:   database/seeders/UserSeeder.php 	modified:   routes/api.php 	modified:   tests/Feature/UserTest.php 	modified:   tests/TestCase.php

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertNotNull;
 
 class UserTest extends TestCase
 {
@@ -55,6 +59,53 @@ class UserTest extends TestCase
             "errors" => [
                 "username" => [
                     "The username has already been taken."
+                ]
+            ]
+        ]);
+    }
+
+    public function testLoginSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+        $this->post('api/users/login', [
+            'username' => 'test',
+            'password' => 'test',
+        ])->assertStatus(200)->assertJson([
+            "data" => [
+                "id" => 1,
+                "username" => "test",
+                "name" => "test"
+            ]
+        ]);
+
+        $user = User::where('username', 'test')->first();
+        assertNotNull($user->token);
+    }
+
+    public function testLoginFailedUsernameNotFound()
+    {
+        $this->post('api/users/login', [
+            'username' => 'test2',
+            'password' => 'test',
+        ])->assertStatus(401)->assertJson([
+            "errors" => [
+                "message" => [
+                    "username or password inccorect"
+                ]
+            ]
+        ]);
+    }
+
+    public function testLoginPasswordWrong()
+    {
+        $this->seed([UserSeeder::class]);
+        $this->post('api/users/login', [
+            'username' => 'test',
+            'password' => 'test2',
+        ])->assertStatus(401)->assertJson([
+            "errors" => [
+                "message" => [
+                    "username or password inccorect"
                 ]
             ]
         ]);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,6 +13,6 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->artisan('migrate:fresh');
-        // $this->artisan('db:seed');
+        $this->artisan('db:seed');
     }
 }
